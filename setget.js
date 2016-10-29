2e3b32baf5806d757b2b062dce7cd5865b8f9ff8
Steve Binder <email@stevebinder.com>
2016-10-29 18:08:02 -0400

    Update setget.js

--- setget.js
+++ setget.js
@@ -1,47 +1,74 @@
-function set(key, value) {
-    if (key === undefined || key === null) {
-        throw new Error("missing key for localStorage set");
-    }
-    if (key === "*") {
-        localStorage.clear();
-    }
-    else if (value === undefined)
-    {
-        delete localStorage[key];
-    }
-    else if (typeof(value) === "object") {
-        try {
-            localStorage[key] = JSON.stringify(value);
-        }
-        catch (e) {
-            throw new Error("cannot set localStorage key with invalid json");
-        }
-    }
-    else {
-        localStorage[key] = value;
-    }
-}
+!(function() {
 
-function get(key, defaultValue) {
-    if (key === undefined || key === null) {
-        throw new Error("missing key for localStorage retrieval");
-    }
-    if (key === "*") {
-        var obj = {};
-        for (var i in localStorage) {
-            obj[i] = get(i);
+    var _DEBOUNCE_TIMEOUT = 0;
+    var _debouncers = {};
+
+    window.set = set;
+    window.get = get;
+
+    function set(key, value, debounce) {
+        if (debounce) {
+            _debounce(set, [key, value], debounce);
+            return;
         }
-        return obj;
+        if (key === undefined || key === null) {
+            throw new Error("missing key for localStorage set");
+        }
+        if (key === "*") {
+            localStorage.clear();
+        }
+        else if (value === undefined)
+        {
+            delete localStorage[key];
+        }
+        else if (typeof(value) === "object") {
+            try {
+                localStorage[key] = JSON.stringify(value);
+            }
+            catch (e) {
+                throw new Error("cannot set localStorage key with invalid json");
+            }
+        }
+        else {
+            localStorage[key] = value;
+        }
     }
-    var value = localStorage[key];
-    if (value === undefined) {
-        return defaultValue;
+
+    function get(key, defaultValue) {
+        if (key === undefined || key === null) {
+            throw new Error("missing key for localStorage retrieval");
+        }
+        if (key === "*") {
+            var obj = {};
+            for (var i in localStorage) {
+                obj[i] = get(i);
+            }
+            return obj;
+        }
+        var value = localStorage[key];
+        if (value === undefined) {
+            return defaultValue;
+        }
+        if (value && /^[\{|\[]/.test(value[0])) {
+            try {
+                value = JSON.parse(value);
+            }
+            catch (e) {
+                return defaultValue;
+            }
+        }
+        return value;
     }
-    try {
-        value = JSON.parse(value);
+
+    function _debounce(method, args, time) {
+        var name = (method + "").match(/^function (.+?)\(/)[1];
+        time = isNaN(time * 1) ? _DEBOUNCE_TIMEOUT : time;
+        if (_debouncers[name]) {
+            clearTimeout(_debouncers[name]);
+        }
+        _debouncers[name] = setTimeout(function() {
+            delete _debouncers[name];
+            method.apply(null, args);
+        }, time);
     }
-    catch (e) {
-        return defaultValue;
-    }
-    return value;
-}
+})();
